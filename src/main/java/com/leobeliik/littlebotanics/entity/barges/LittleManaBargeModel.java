ec7944e5ec6ff8d567170b71dcd60e7efa04b4a4
Leo <LeoBrac23@gmail.com>
2023-01-17 14:03:25 -0300

    barge model

--- src/main/java/com/leobeliik/littlebotanics/entity/barges/LittleManaBargeModel.java
+++ src/main/java/com/leobeliik/littlebotanics/entity/barges/LittleManaBargeModel.java
@@ -8,7 +8,6 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
-
 import static com.leobeliik.littlebotanics.LittleBotanics.MODID;
 
 public class LittleManaBargeModel extends EntityModel<LittleManaBargeEntity> {
@@ -27,26 +26,23 @@ public class LittleManaBargeModel extends EntityModel<LittleManaBargeEntity> {
         PartDefinition partdefinition = meshdefinition.getRoot();
 
         partdefinition.addOrReplaceChild("pool", CubeListBuilder.create()
-                        .texOffs(32, 12).addBox(-8.0F, -14.0F, -8.0F, 2.0F, 8.0F, 16.0F, new CubeDeformation(0.0F)) //l
-                        .texOffs(32, 12).addBox(6.0F, -14.0F, -8.0F, 2.0F, 8.0F, 16.0F, new CubeDeformation(0.0F)) //l
-                        .texOffs(34, 26).addBox(-5.0F, -14.0F, -8.0F, 10.0F, 8.0F, 2.0F, new CubeDeformation(1F, 0F, 0F)) //s
-                        .texOffs(34, 26).addBox(-5.0F, -14.0F, 6.0F, 10.0F, 8.0F, 2.0F, new CubeDeformation(1F, 0F, 0F)) //s
-                        .texOffs(24, 16).addBox(-6.0F, -8.0F, -6.0F, 12.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), //b
-                PartPose.offset(0.0F, 24.0F, 0.0F));
+                        .texOffs(0, 19).addBox(-8.0F, -14.0F, -8.0F, 2.0F, 8.0F, 16.0F, new CubeDeformation(0.0F)) //l
+                        .texOffs(0, 19).addBox(6.0F, -14.0F, -8.0F, 2.0F, 8.0F, 16.0F, new CubeDeformation(0.0F)) //l
+                        .texOffs(2, 33).addBox(-5.0F, -14.0F, -8.0F, 10.0F, 8.0F, 2.0F, new CubeDeformation(1F, 0F, 0F)) //s
+                        .texOffs(2, 33).addBox(-5.0F, -14.0F, 6.0F, 10.0F, 8.0F, 2.0F, new CubeDeformation(1F, 0F, 0F)) //s
+                        .texOffs(-8, 23).addBox(-6.0F, -8.0F, -6.0F, 12.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), //b
+                PartPose.offset(0.0F, 3.0F, 0.0F));
 
-        PartDefinition barge = partdefinition.addOrReplaceChild("barge", CubeListBuilder.create() //TODO
-                .texOffs(0, 0).addBox(-6.0F, -27.0F, -7.0F, 12.0F, 5.0F, 14.0F, new CubeDeformation(0.0F))
-                .texOffs(38, 5).addBox(-8.0F, -29.0F, -7.0F, 2.0F, 4.0F, 14.0F, new CubeDeformation(0.0F))
-                .texOffs(28, 43).addBox(-6.0F, -29.0F, -9.0F, 12.0F, 4.0F, 2.0F, new CubeDeformation(0.0F))
-                .texOffs(26, 25).addBox(6.0F, -29.0F, -7.0F, 2.0F, 4.0F, 14.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 41).addBox(-6.0F, -29.0F, 7.0F, 12.0F, 4.0F, 2.0F, new CubeDeformation(0.0F)),
+        PartDefinition barge = partdefinition.addOrReplaceChild("barge", CubeListBuilder.create()
+                        .texOffs(0, 0).addBox(-6.0F, -27.0F, -7.0F, 12.0F, 5.0F, 14.0F, new CubeDeformation(0.0F)),
                 PartPose.offset(0.0F, 23.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 128, 128);
     }
 
     @Override
-    public void setupAnim(LittleManaBargeEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {}
+    public void setupAnim(LittleManaBargeEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+    }
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
